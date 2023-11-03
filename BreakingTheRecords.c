276ac569a4760fb3825ac64283170a65762b4db7
Samyuktha Busireddy <102577618+SamyukthaBusireddy@users.noreply.github.com>
2023-11-03 22:33:16 +0530

    Breaking The Records

--- BreakingTheRecords.c
+++ BreakingTheRecords.c
@@ -1,8 +1,30 @@
 #include<stdio.h>
-int main{
-    int n;
+int main()
+{
+    int n,a[1000],c=0,d=0;
     scanf("%d",&n);
-    int a[n];
-    for(int i=
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
     }
+  int  max=a[0];
+    for(int i=0;i<n;i++)
+    {
+        if(a[i]>max)
+        {
+            max=a[i];
+            c++;
+        } 
+    }
+    int min=a[0];
+    for(int i=0;i<n;i++)
+    {
+        if(a[i]<min)
+        {
+            min=a[i];
+            d++;
+        } 
+    }
+    printf("%d %d",c,d);
+    
 }
